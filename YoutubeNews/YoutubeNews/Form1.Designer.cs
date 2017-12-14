1f9e4099a088a5a488acab89d1a4f417f2152ce1
Trần Tâm <tam.tran@softfoundry.com>
2017-12-14 18:06:20 +0700

    + Fisrt commit

---
+++ YoutubeNews/YoutubeNews/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace YoutubeNews
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.txtImage = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(45, 112);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(648, 349);
+            this.txtText.TabIndex = 0;
+            // 
+            // txtImage
+            // 
+            this.txtImage.BackColor = System.Drawing.SystemColors.Window;
+            this.txtImage.Font = new System.Drawing.Font("Arial", 12.22642F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImage.Location = new System.Drawing.Point(45, 40);
+            this.txtImage.Name = "txtImage";
+            this.txtImage.Size = new System.Drawing.Size(648, 28);
+            this.txtImage.TabIndex = 1;
+            this.txtImage.TextChanged += new System.EventHandler(this.txtImage_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Image url";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(45, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Text";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Font = new System.Drawing.Font("Arial", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.Location = new System.Drawing.Point(571, 475);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(122, 39);
+            this.btnCreate.TabIndex = 3;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 524);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtImage);
+            this.Controls.Add(this.txtText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.TextBox txtImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
---
+++ YoutubeNews/YoutubeNews/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeNews
+{
+    public partial class Form1 : Form
+    {
+        private string strImagePath = "image.jpg";
+        //private string strCreateCommand;
+        //private string strCurentPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void txtImage_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if(this.txtImage.Text != "")
+            {
+                try
+                {
+                    saveImage();
+                    saveTextToFile();
+                    createVideo();
+                    insertTextToVideo();
+                    MessageBox.Show("Complete Create video!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.ToString(), "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }     
+            }
+            else
+            {
+                MessageBox.Show("Please input url image!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void saveImage()
+        {
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(new Uri(this.txtImage.Text), strImagePath);
+            }
+        }
+
+        private void saveTextToFile()
+        {
+            using (var sw = new StreamWriter(File.Open("input.txt", FileMode.Create), Encoding.GetEncoding("UTF-8")))
+            {
+                for (int i = 0; i < txtText.Text.Length; i++)
+                {
+                    if( i % 14 == 0)
+                    {
+                        if(txtText.Text.Length > (i + 14) )
+                        {
+                            sw.WriteLine(txtText.Text.Substring(i , 14));
+                        }
+                        else
+                        {
+                            sw.WriteLine(txtText.Text.Substring(i, txtText.Text.Length - i));
+                        }
+                    }
+                }
+            }
+        }
+        private void createVideo()
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "ffmpeg.exe";
+            // proc.StartInfo.Arguments = "-loop 1 -y -i image.jpg  -vf \"scale = trunc(iw / 2) * 2:trunc(ih / 2) * 2\" -c:v libx264 -t 30 -pix_fmt yuv420p out.mp4";
+            proc.StartInfo.Arguments = "-loop 1 -y -i image.jpg  -vf \"scale = 640:480\" -c:v libx264 -t 180 -pix_fmt yuv420p out.mp4";
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.UseShellExecute = false;
+            if (!proc.Start())
+            {
+                MessageBox.Show("Error starting");
+                return;
+            }
+            StreamReader reader = proc.StandardError;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            proc.Close();
+        }
+        private void insertTextToVideo()
+        {
+            Process proc = new Process();
+            proc.StartInfo.FileName = "ffmpeg.exe";
+            proc.StartInfo.Arguments = "-y -i out.mp4 -vf \"[in]drawtext = textfile = 'input.txt':fontfile = ARIALUNI.TTF:x = 18:y = h - 30 * t:fontcolor = ffffff:fontsize = 36:shadowx = 2:shadowy = 2[out]\"  final.mp4";
+            proc.StartInfo.RedirectStandardError = true;
+            proc.StartInfo.UseShellExecute = false;
+            if (!proc.Start())
+            {
+                MessageBox.Show("Error starting");
+                return;
+            }
+            StreamReader reader = proc.StandardError;
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+            proc.Close();
+        }
+    }
+}
